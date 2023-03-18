fa3d2d9bac129937bd75b0bc5f8ab4a8266d606e
walwizard <goneisgone@hotmail.com>
2023-03-17 23:28:11 -0300

    Criação de componente HOME, e atualização do projeto

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Home.css";
+
+import backround from "../images/bacnground-inicio.png"
+
+const Home  = () => {
+    return (
+        <div className="home" style={{backgroundImage: `url=(${backround})`}}>
+
+        </div>
+    )
+}
+
+export default Home
